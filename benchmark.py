d61d631bd399b8affc800ff528bc226973d6c73f
Elliot Gorokhovsky <elliot.gorokhovsky@gmail.com>
2016-10-09 23:52:30 -0600

    Updated benchmark script to add 1e3 test

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,24 @@ from fastlist import FastList
 import random; random.seed(42)
 from time import time
 
+print("*** 1e3 ints ***")
+L = [random.randrange(int(1e3)) for _ in range(int(1e3))]
+
+F = FastList(L[:])
+start = time(); F.fastsort(); print("F.fastsort():", time()-start)
+
+F = FastList(L[:])
+start = time(); F.sort(); print("F.sort():", time()-start)
+
+print("*** 1e3 strings ***")
+L = list(map(str, L))
+
+F = FastList(L[:])
+start = time(); F.fastsort(); print("F.fastsort():", time()-start)
+
+F = FastList(L[:])
+start = time(); F.sort(); print("F.sort():", time()-start)
+
 print("*** 1e7 ints ***")
 L = [random.randrange(int(1e7)) for _ in range(int(1e7))]
 
